fa743180b3e4bb40755590c813bd42eefcea0412
sangwon-ryu <fbtkdals2@naver.com>
2025-11-24 16:04:42 +0900

    feat: User Aggregate 구현 (Green)

---
+++ domain/src/main/java/com/ryuqq/authhub/domain/user/UserType.java
@@ -0,0 +1,16 @@
+package com.ryuqq.authhub.domain.user;
+
+/**
+ * 사용자 유형
+ */
+public enum UserType {
+    /**
+     * 공개 사용자 (일반 사용자)
+     */
+    PUBLIC,
+
+    /**
+     * 내부 사용자 (시스템 관리자, 운영자)
+     */
+    INTERNAL
+}
