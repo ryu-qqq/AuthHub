9352e401fa7204540b3bcac5f8450e6e00b4c367
sangwon-ryu <fbtkdals2@naver.com>
2026-02-11 14:27:59 +0900

    fix: InternalUserContextController 반환 타입을 ResponseEntity로 래핑
    컨벤션에 맞게 ApiResponse<T> 직접 반환에서 ResponseEntity<ApiResponse<T>>로 수정

--- adapter-in/rest-api/src/main/java/com/ryuqq/authhub/adapter/in/rest/internal/controller/InternalUserContextController.java
+++ adapter-in/rest-api/src/main/java/com/ryuqq/authhub/adapter/in/rest/internal/controller/InternalUserContextController.java
@@ -10,7 +10,9 @@ import com.ryuqq.authhub.application.token.dto.response.MyContextResponse;
 import com.ryuqq.authhub.application.token.port.in.query.GetMyContextUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,10 +57,15 @@ public class InternalUserContextController {
      */
     @GetMapping(USER_CONTEXT)
     @Operation(summary = "사용자 컨텍스트 조회", description = "내부 서비스가 사용자의 전체 컨텍스트 정보를 조회합니다.")
-    public ApiResponse<UserContextApiResponse> getUserContext(
+    @ApiResponses({
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "200",
+                description = "조회 성공")
+    })
+    public ResponseEntity<ApiResponse<UserContextApiResponse>> getUserContext(
             @Parameter(description = "사용자 ID", required = true) @PathVariable String userId) {
         MyContextResponse result = getMyContextUseCase.execute(userId);
         UserContextApiResponse response = mapper.toApiResponse(result);
-        return ApiResponse.ofSuccess(response);
+        return ResponseEntity.ok(ApiResponse.ofSuccess(response));
     }
 }
